9a7b544999a40d394fd5a2ba9d7982bfca9d94fe
Jae Yoo <pppjqqq@gmail.com>
2020-05-11 21:27:42 -0700

    Finished bare minimum requirements and working on parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,48 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json[0] === '"' && json[json.length - 1] === '"') {
+    return json;
+  }
+  if (json[0] === '[' && json[json.length - 1] === ']') {
+    if (json.length === 2) {
+      return [];
+    }
+    const result = [];
+    json = json.slice(1, json.length - 1);
+    let elements = json.split(',');
+    let i = 0;
+    while (i < elements.length) {
+      result.push(parseJSON(elements[i]));
+      i++;
+    }
+    return result;
+  }
+  if (json[0] === '{' && json[json.length - 1] === '}') {
+    if (json.length === 2) {
+      return {};
+    }
+    const result = {};
+    json = json.slice(1, json.length - 1);
+    let keysValues = json.split(',');
+    let j = 0;
+    while (j < keysValues.length) {
+      let keyValuePair = keysValues[j].split(':');
+      result[parseJSON(keyValuePair[0])] = parseJSON(keyValuePair[1]);
+      j++;
+    }
+    return result;
+  }
+  if (json === 'true') {
+    return true;
+  }
+  if (json === 'false') {
+    return false;
+  }
+  if (json === 'null') {
+    return null;
+  }
+  if (!isNaN(Number(json))) {
+    return Number(json);
+  }
 };
